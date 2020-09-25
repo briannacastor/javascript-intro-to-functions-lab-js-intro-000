4ab964e846fe448c3944b65bd52cdb4f5caab1bc
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-25 21:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string){
 function logWhisper(string){
   console.log('hello')
 }
+function sayHiToGrandma(string){
+  if (string.toLowerCase()===string
+){
+  return 'I can\'t hear you!';
+}
+if (string.toUpperCase()===string
+){
+  return 'YES INDEED!';
+  }
+  if (string==="I love you, Grandma.")
+  {
+    return "Ilove you, too";
+  }
+  
