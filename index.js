b085db58f5325592a30046be0026ca968c8c147d
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-09-25 20:46:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function shout(string){
+  return string.toUpperCase('hello')
+}
+function whisper(string){
+  return string.toLowerCase('HELLO')
+}
+function logShout(string){
+  console.log('HELLO')
+}
+function logWhisper(string){
+  console.log('hello')
+}
